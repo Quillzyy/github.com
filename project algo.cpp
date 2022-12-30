7886855368c3f118f91cea653fd9a0688b8491f4
cornelius Y.R <cornelius.yuli.rosdianto.tik22@mhsw.pnj.ac.id>
2022-12-30 23:32:47 +0700

    Penambahan nama

---
+++ project algo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void nama(string nama){
+cout<<nama;	
+}
+int main(){
+cout<<"Ketua kelompok: \n";
+nama("saqil");cout<<endl;cout<<endl;	
+cout<<"Nama kelompok: ";cout<<endl;	
+nama("Ammar"); cout<<endl;
+nama("Kevin"); cout<<endl;
+nama("Brian"); cout<<endl;
+nama("jopi"); cout<<endl;	
+nama("cornelius"); cout<<endl;
+		
+return 0;	
+}
