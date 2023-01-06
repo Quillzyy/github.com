730a5010aefe7bfdf1ceca453f877b22055c22bd
Alonzo0604 <bakarakevin679@gmail.com>
2023-01-06 15:33:17 +0700

    Penambahan anggota

---
+++ project qil.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include <cstdlib>
+#include<conio.h>
+
+#ifdef _WIN32
+	#define clear "cls"
+#else
+	#define clear "clear"
+#endif
+using namespace std;
+
+
+
+
+int main(){
+string IN;
+char salah;
+string NIM, Nama;
+do{
+
+
+
+//cout<<"==========List NIM yang terdaftar========== \n";
+
+cout<<"LOGIN\n";
+
+cout<<"Masukan NIM: 2207421";
+cin>>NIM;
+
+
+	if(NIM=="047"){
+		Nama="Abdurrahman Ammar Ihsan \n";
+		
+	}else if (NIM=="059"){
+		Nama="Corneius Yuli Rosdianto"
+	}else if (NIM=="032"){
+		Nama="Kevin Alonzo Manuel Bakara"
+	}else if (NIM=="057"){
+		Nama="Shaquille Afriza Hidayat"
+	}else if (NIM=="056"){
+		Nama="Muhammad Brian Azura Nixon"	
+	}else if (NIM=="051"){
+		Nama="Jonatan Victorian Wijaya"	
+	}else{
+		cout<<"Maaf NIM tidak terdaftar \n";
+		salah='y';
+		
+	}
+getch();
+IN="SELAMAT DATANG";
+salah=='n';	
+system(clear);
+}while (salah=='y');
+cout<<IN<<endl;
+cout<<"Nama 	: "<<Nama<<endl;
+cout<<"NIM	: 2207421"<<NIM<<endl;
+cout<<"Kelas	: TMJ 1B"<<endl;
+
+	
+	
+	
+	
+return 0;	
+}
