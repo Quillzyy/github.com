a5b882871c3358172e4d235437b81b479659730c
rahillahanami <rahillahanami2@gmail.com>
2023-01-06 15:16:11 +0700

    test

--- project qil.cpp
+++ project qil.cpp
@@ -15,33 +15,36 @@ using namespace std;
 int main(){
 string IN;
 char salah;
+string NIM, Nama;
 do{
 
-string NIM;
+
 
 //cout<<"==========List NIM yang terdaftar========== \n";
 
 cout<<"LOGIN\n";
 
-cout<<"Masukan NIM: ";
+cout<<"Masukan NIM: 2207421";
 cin>>NIM;
 
 
-if(NIM=="047"){
-IN="SELAMAT DATANG Abdurrahman Ammar Ihsan \n";	
-salah=='n';
-}
-else{
-cout<<"Maaf NIM tidak terdaftar \n";
-salah='y';
-
-getch();	
-}
+	if(NIM=="047"){
+		Nama="Abdurrahman Ammar Ihsan \n";
+		IN="SELAMAT DATANG";
+		salah=='n';
+	} else{
+		cout<<"Maaf NIM tidak terdaftar \n";
+		salah='y';
+		getch();	
+	}
+	
 system(clear);
 }while (salah=='y');
-cout<<endl;
+cout<<IN<<endl;
+cout<<"Nama 	: "<<Nama<<endl;
+cout<<"NIM	: 2207421"<<NIM<<endl;
+cout<<"Kelas	: TMJ 1B"<<endl;
 
-cout<<IN;
 	
 	
 	
