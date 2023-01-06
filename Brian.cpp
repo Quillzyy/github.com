a4b4d491073e7ffa793502c210758346b719a4e9
zrnxn <brianazura04@gmail.com>
2023-01-06 16:06:09 +0700

    void Brian

--- Brian.cpp
+++ Brian.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Hai";
+void brian(){
+    cout << Nama << endl;
+    cout << "2207421056" << endl;
+    cout << "Teknik Informatika dan Komputer";
+    cout << "Teknik Multimedia dan Jaringan"; 
 }
