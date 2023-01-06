80cea322e143294b735f82ae00f15dfdf00e5d7c
Alonzo0604 <bakarakevin679@gmail.com>
2023-01-06 16:00:47 +0700

    Update lagi brok

--- project qil.cpp
+++ project qil.cpp
@@ -14,8 +14,8 @@ using namespace std;
 
 
 int main(){
-string IN;
 char salah;
+int pil;
 string NIM, Nama;
 do{
 
@@ -23,47 +23,50 @@ do{
 
 //cout<<"==========List NIM yang terdaftar========== \n";
 
+system(clear);
 cout<<"LOGIN\n";
-
 cout<<"Masukan NIM: 2207421";
 cin>>NIM;
 
 
 	if(NIM=="047"){
-		Nama="Abdurrahman Ammar Ihsan \n";
+		Nama="Abdurrahman Ammar Ihsan ";
 	}else if (NIM=="059"){
-		Nama="Corneius Yuli Rosdianto"
+		Nama="Corneius Yuli Rosdianto";
 	}else if (NIM=="032"){
-		Nama="Kevin Alonzo Manuel Bakara"
+		Nama="Kevin Alonzo Manuel Bakara";
 	}else if (NIM=="057"){
-		Nama="Shaquille Afriza Hidayat"
+		Nama="Shaquille Afriza Hidayat";
 	}else if (NIM=="056"){
-		Nama="Muhammad Brian Azura Nixon"	
+		Nama="Muhammad Brian Azura Nixon";	
 	}else if (NIM=="051"){
-		Nama="Jonatan Victorian Wijaya"	
+		Nama="Jonatan Victorian Wijaya";
 	}else{
 		cout<<"Maaf NIM tidak terdaftar \n";
 		salah='y';
     }
-getch();
-IN="SELAMAT DATANG";
+
+
 salah=='n';		
 system(clear);
 }while (salah=='y');
 cout<<"SELAMAT DATANG"<<endl;
+getch();
 cout<<"Nama 	: "<<Nama<<"\t |\n";
 cout<<"NIM	: 2207421"<<NIM<<"\t\t\t |\n";
 cout<<"Kelas	: TMJ 1B"<<"\t\t\t |\n";
 cout << "_________________________________________|";
 
 
+cout << endl ;
 cout << "1. Lihat Profil" << endl;
 cout << "2. Ujian" << endl;
 cout << "3. Lihat Nilai" << endl;
-cout << "4. Logout" << endl;
-	
-	
-	
+cout 
+<< "4. Logout" << endl;
+cout << "Pilihan Anda : ";
+cin >> pil;
+
 	
 return 0;	
 }
