3eaa003b2a7ef4eec07db5e829bc9c10f09e0440
rikzakmal <rikzaakmalhabibi@gmail.com>
2023-01-06 15:40:46 +0700

    tambah switch

--- project qil.cpp
+++ project qil.cpp
@@ -1,58 +1,64 @@
 #include<iostream>
-#include <cstdlib>
+#include<cstdlib>
 #include<conio.h>
-
-#ifdef _WIN32
-	#define clear "cls"
-#else
-	#define clear "clear"
-#endif
 using namespace std;
 
+string NIM, Nama;
 
-
+void ammar(){
+	cout << Nama << endl;
+	cout << 
+}
 
 int main(){
-string IN;
-char salah;
-string NIM, Nama;
-do{
+	do{
+		char salah;
 
 
+		//cout<<"==========List NIM yang terdaftar========== \n";
 
-//cout<<"==========List NIM yang terdaftar========== \n";
+		cout<<"LOGIN\n";
 
-cout<<"LOGIN\n";
-
-cout<<"Masukan NIM: 2207421";
-cin>>NIM;
-
-
-	if(NIM=="047"){
-		Nama="Abdurrahman Ammar Ihsan";
-		salah=='n';
-	} else{
-		cout<<"Maaf NIM tidak terdaftar \n";
-		salah='y';
-		getch();	
-	}
-	
-system(clear);
-}while (salah=='y');
-cout<<"SELAMAT DATANG"<<endl;
-cout<<"Nama 	: "<<Nama<<"\t |\n";
-cout<<"NIM	: 2207421"<<NIM<<"\t\t\t |\n";
-cout<<"Kelas	: TMJ 1B"<<"\t\t\t |\n";
-cout << "_________________________________________|";
+		cout<<"Masukan NIM: 2207421";
+		cin>>NIM;
 
 
-cout << "1. Lihat Profil" << endl;
-cout << "1. Lihat Profil" << endl;
-cout << "1. Lihat Profil" << endl;
-cout << "1. Lihat Profil" << endl;
-	
-	
-	
-	
-return 0;	
+		if(NIM=="047"){
+			Nama="Abdurrahman Ammar Ihsan";
+			salah=='n';
+		}
+		else{
+			cout<<"Maaf NIM tidak terdaftar \n";
+			salah='y';
+			getch();	
+		}
+			
+		system("CLS");
+	} while (salah=='y');
+
+	do{
+		int pilihan;
+		char salah;
+		cout <<"SELAMAT DATANG"<<endl;
+		cout <<"Nama 	: "<< Nama <<"\t |\n";
+		cout <<"NIM	: 2207421"<< NIM <<"\t\t\t |\n";
+		cout <<"Kelas	: TMJ 1B"<< "\t\t\t |\n";
+		cout << "_________________________________________|" << endl;
+		cout << "1. Lihat Profil" << endl;
+		cout << "2. Ujian" << endl;
+		cout << "3. Lihat Nilai" << endl;
+		cout << "4. Logout" << endl;
+		cout << "Masukkan Pilihan : ";
+
+		cin >> pilihan;
+
+		switch(pilihan){
+			case 1:
+			if(NIM=="047"){
+				ammar();
+			}
+
+		}
+
+	} while (salah == "y")
 }
